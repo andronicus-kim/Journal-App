71545a4f3fbba2343df2488da18a229f6ba22c6c
andronicus-kim <androkim.kim7@gmail.com>
2018-07-01 12:55:24 +0300

    adds new entry and entry details activities

--- app/src/main/java/com/andronicus/journalapp/AddNewEntry.java
+++
@@ -1,7 +0,0 @@
-package com.andronicus.journalapp;
-
-/**
- * Created by andronicus on 7/1/2018.
- */
-public class AddNewEntry {
-}
--- app/src/main/java/com/andronicus/journalapp/EntryDetailsActivity.java
+++ app/src/main/java/com/andronicus/journalapp/EntryDetailsActivity.java
@@ -3,11 +3,11 @@ package com.andronicus.journalapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Entries extends AppCompatActivity {
+public class EntryDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entries);
+        setContentView(R.layout.activity_entry_details);
     }
 }
