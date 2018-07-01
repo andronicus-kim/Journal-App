75aace7f18e3a0ea4aa2db90b54f732581238538
andronicus-kim <androkim.kim7@gmail.com>
2018-07-01 11:51:08 +0300

    initial commit

---
+++ app/src/main/java/com/andronicus/journalapp/AddJournal.java
@@ -0,0 +1,13 @@
+package com.andronicus.journalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddJournal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_journal);
+    }
+}
