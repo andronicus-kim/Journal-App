96dad6b738894fffbb20149e31bb7936ad997f5a
andronicus-kim <androkim.kim7@gmail.com>
2018-07-01 14:39:29 +0300

    adds entry dao

---
+++ app/src/main/java/com/andronicus/journalapp/EntryDao.java
@@ -0,0 +1,28 @@
+package com.andronicus.journalapp;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Created by andronicus on 7/1/2018.
+ */
+@Dao
+public interface EntryDao {
+
+    @Query("SELECT * FROM entries")
+    List<Entry> getAll();
+
+    @Query("SELECT * FROM entries WHERE id=:id")
+    Entry getOne(String id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void save(Entry entry);
+
+    @Update
+    int update(Entry entry);
+}
