971606014127f239c2fb4821ca07df79a9cf3dc2
andronicus-kim <androkim.kim7@gmail.com>
2018-07-01 13:14:48 +0300

    bug fix

--- app/src/main/java/com/andronicus/journalapp/AddNewEntryActivity.java
+++ app/src/main/java/com/andronicus/journalapp/AddNewEntryActivity.java
@@ -1,10 +1,15 @@
 package com.andronicus.journalapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AddNewEntryActivity extends AppCompatActivity {
 
+    public static Intent newIntent(Context context){
+        return new Intent(context,AddNewEntryActivity.class);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/andronicus/journalapp/EntriesActivity.java
+++ app/src/main/java/com/andronicus/journalapp/EntriesActivity.java
@@ -1,13 +1,25 @@
 package com.andronicus.journalapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class EntriesActivity extends AppCompatActivity {
+public class EntriesActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private FloatingActionButton mFloatingActionButton;
+    private RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entries);
+        mFloatingActionButton = findViewById(R.id.fab);
+        mFloatingActionButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(AddNewEntryActivity.newIntent(EntriesActivity.this));
     }
 }
