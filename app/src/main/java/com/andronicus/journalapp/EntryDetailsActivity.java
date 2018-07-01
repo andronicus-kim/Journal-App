e4d24e4106881e9cca504fed3548641c0c4c6fd9
andronicus-kim <androkim.kim7@gmail.com>
2018-07-01 19:11:45 +0300

    inflates menu in entry detail activity

--- app/src/main/java/com/andronicus/journalapp/EntryDetailsActivity.java
+++ app/src/main/java/com/andronicus/journalapp/EntryDetailsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class EntryDetailsActivity extends AppCompatActivity {
 
@@ -21,5 +22,15 @@ public class EntryDetailsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.entry_details,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_edit){
+
+        }else if (item.getItemId() == R.id.action_delete){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
